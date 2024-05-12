d4b5fa9661e32356abd07f378f59a548e2a472e6
Jasper Wong <wlcjasper@gmail.com>
2024-05-12 19:29:21 +0100

    Rename App.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
 
-import App from "./app.jsx";
+import App from "./App.jsx"
 
-import { ContextProvider } from "./context.jsx";
+import { ContextProvider } from "./context.jsx"
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
     <ContextProvider>
       <App />
     </ContextProvider>
-  </React.StrictMode>,
-);
+  </React.StrictMode>
+)
